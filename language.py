947fa46cc8af33057b46407a4c8b35815f6a13dd
StardustGogeta <liam.gerst@gmail.com>
2017-06-02 19:22:02 -0400

    Adding the main file and key samples
    The samples include some choice words from each language, the song
    "Fotografia", the Spiderman 1967, Avatar: The Last Airbender, and Romeo
    and Juliet intros, and the Song of the Dragonborn.

---
+++ language.py
@@ -0,0 +1,136 @@
+import math, re, samples
+from collections import Counter
+
+# How to use:
+# - Create language with Language class
+# - Train individual words or sentences with Language.teach()
+# - Evaluate individual words or sentences with Language.evaluate()
+# - Use LanguageCollection.evaluate() to test multiple languages at once
+#   and receive the name of the best-fitting language
+
+class LanguageCollection():
+    def __init__(self, langs):
+        self.langs = langs
+        
+    def evaluate(self, sample): # Return best-fit language and score
+        best = ['',0]
+        for lang in self.langs:
+            c = lang.evaluate(sample)
+            if c > best[1]:
+                best[1] = c
+                best[0] = lang.lang
+        return best[0]+' with a confidence of '+str(best[1])
+
+    def printFullEval(self,sample): # Print all languages, sorted by score.
+        print('\n'.join(l[0]+' with a confidence of '+str(l[1]) for l in self.fullEvaluate(sample)))
+        
+    def fullEvaluate(self, sample): # Return all languages, sorted by score.
+        langs = []
+        for lang in self.langs:
+            c = lang.evaluate(sample)
+            langs.append([lang.lang,c])
+        langs.sort(key = lambda x: x[1])
+        return langs[::-1]
+            
+# Wrapper Language class spawns Neurons based on configuration
+class Language():
+    def __init__(self, lang, config = []):
+        self.lang = lang
+        self.brain = []
+        self.config = config if config else [('pre',3),('suf',3),('mid',2),('pre',2),('mid',1),('suf',4),
+                                             ('suf',2),('mid',3),('mid',4),('pre',1),('suf',1),('pre',4)]
+        for c in self.config:
+            self.brain += [NeuronGroup(*c)]
+        self.brain += [NeuronFrequency()]
+            
+    def teach(self, sample):
+        sample = sample.lower()
+        for word in re.findall(r"[a-z]+",sample):
+            for neuron in self.brain:
+                neuron.teach(word)
+            
+    def evaluate(self, sample):
+        sample = sample.lower()
+        con = [0,0]
+        # Finding the average in one pass, rather than keeping a list.
+        for word in re.findall(r"[a-z]+",sample):
+            for neuron in self.brain:
+                c = neuron.evaluate(word)
+                con[0] = (con[0]*con[1]+c)/(con[1]+1)
+                con[1] += 1
+        return con[0]
+    
+# Parent class for all neuron types:
+class Neuron():
+    def __init__(self):
+        self.lib = {}
+        
+# Subclass of Neuron built for groups of letters:
+class NeuronGroup(Neuron):
+    def __init__(self,location, length):
+        self.location = location
+        self.length = length
+        Neuron.__init__(self)
+
+    def getSubstr(self,sample):
+        if self.location == 'pre':
+            return sample[:self.length]
+        elif self.location == 'suf':
+            return sample[-self.length:]
+        elif self.location == 'mid':
+            return sample[int((len(sample)-self.length)/2):int((len(sample)+self.length)/2)]
+        
+    def teach(self, sample):
+        sampleSubstr = self.getSubstr(sample) 
+        if sampleSubstr in self.lib:
+            self.lib[sampleSubstr] += 1
+        else:
+            self.lib[sampleSubstr] = 1
+            
+    def evaluate(self, sample):
+        sampleSubstr = self.getSubstr(sample)
+        if sampleSubstr in self.lib:
+            confidence = 1 - math.e**-self.lib[sampleSubstr]
+        else:
+            confidence = 0
+        return confidence
+        
+# Subclass of Neuron built for checking letter frequency: 
+class NeuronFrequency(Neuron):
+    def getFreqs(self,sample):
+        return ''.join(x[0] for x in Counter(sample).most_common(3))
+        
+    def teach(self, sample):
+        freqs = self.getFreqs(sample)
+        if freqs in self.lib:
+            self.lib[freqs] += 1
+        else:
+            self.lib[freqs] = 1
+            
+    def evaluate(self, sample):
+        freqs = self.getFreqs(sample)
+        if freqs in self.lib:
+            confidence = 1 - math.e**-self.lib[freqs]
+        else:
+            confidence = 0
+        return confidence
+
+
+# Use theme songs, poems, and books to train AI
+Spanish = Language("Spanish")
+Spanish.teach(samples.SpanishBase)
+Spanish.teach(samples.Spanish1)
+
+English = Language("English")
+English.teach(samples.EnglishBase)
+English.teach(samples.EnglishLevel1)
+English.teach(samples.English1)
+English.teach(samples.English2)
+English.teach(samples.English3)
+
+Dragon = Language("Dragon")
+Dragon.teach(samples.DragonBase)
+Dragon.teach(samples.Dragon1)
+
+L = LanguageCollection([Spanish,English,Dragon])
+# L.evaluate("Insert text here!")
---
+++ samples.py
@@ -0,0 +1,133 @@
+SpanishBase = """
+trabajador lengua palabras traductor traer tratar enviar copiar el perro corre come comera
+yo soy un estudiante muy talentoso llamar inteligente gente simple trabajare hablare
+aplicar cantas dices cortar corregir corazon corriente caliente libre madre padre habre sugiere
+libro hablo miembro cuatro cinco seis siete ocho nueve nieve diez once doce trece pero pinata y
+pez pelota nacer pequeno peso peru penal peor pedir permitir
+"""
+Spanish1 = """
+Cada vez que yo me voy llevo a un lado de mi piel
+Tus fotografías para verlas cada vez
+Que tu ausencia me devora entero el corazón
+Y yo no tengo remedio mas que amarte
+
+Y en la distancia te puedo ver
+Cuando tus fotos me siento a ver
+Y en las estrellas tus ojos ver
+
+Cuando hay un abismo desnudo
+Que se opone entre los dos
+Yo me valgo del recuerdo taciturno de tu voz
+Y de nuevo siento enfermo este corazón
+Que no le queda remedio mas que amarte
+
+Cada vez que te busco te vas
+Y cada vez que te llamo no estas
+Es por eso que debo decir
+Que tu solo en mis fotos estas
+"""
+
+EnglishBase = """
+this is an example of speech synthesis in english please do not fail yes no
+spider man does whatever a can anywhere anytime germany austria language hello
+I he she we our his my your there here never always forever ever window see
+"""
+EnglishLevel1 = """
+question so proletariat choice decision girlfriend barbeque acquaintance stifled
+innovative replicate incinerate obliterate obfuscate disposal available check
+balance calendar display alarm digital electronics physics
+"""
+English1 = """
+Spiderman, Spiderman, 
+Does whatever a spider can
+Spins a web, any size,
+Catches thieves just like flies
+Look Out!
+Here comes the Spiderman.
+
+Is he strong?
+Listen bud,
+He’s got radioactive blood.
+Can he swing from a thread
+Take a look overhead
+Hey, there
+There goes the Spiderman.
+
+In the chill of night
+At the scene of a crime
+Like a streak of light
+He arrives just in time.
+
+Spiderman, Spiderman
+Friendly neighborhood Spiderman
+Wealth and fame
+He’s ignored
+Action is his reward.
+
+To him, life is a great big bang up
+Whenever there’s a hang up
+You’ll find the Spider man.
+"""
+English2 = """
+Water. Earth. Fire. Air.
+Long ago, the four nations lived together in harmony.
+Then, everything changed when the Fire Nation attacked.
+Only the Avatar, master of all four elements, could stop them,
+but when the world needed him most, he vanished.
+A hundred years passed and my brother and I discovered
+the new Avatar, an airbender named Aang.
+And although his airbending skills are great, he has a lot
+to learn before he's ready to save anyone.
+But I believe Aang can save the world.
+"""
+English3 = """
+Two households, both alike in dignity,
+In fair Verona, where we lay our scene,
+From ancient grudge break to new mutiny,
+Where civil blood makes civil hands unclean.
+From forth the fatal loins of these two foes
+A pair of star-cross'd lovers take their life;
+Whole misadventured piteous overthrows
+Do with their death bury their parents' strife.
+"""
+
+DragonBase = """
+Dov wahlaan fah rel sahrot hunne Zin krif horvut se suleyk
+Ok mulaag unslaad zeymah Dahmaan Drem Yol Lok fus ro dah
+raan mir tah laas yah nir mid vur shaan feim zii gron gol hah
+dov od ah viing hun kaal zoor lok vah koor ven gaar nos zun
+haal viik fas ru maar mul qah diiv joor zah frul gaan lah haas
+"""
+Dragon1 = """
+Dovahkiin, Dovahkiin
+naal ok zin los vahriin
+wah dein vokul mahfaeraak ahst vaal!
+Ahrk fin norok paal graan
+fod nust hon zindro zaan
+Dovahkiin, fah hin kogaan mu draal!
+
+Huzrah nu, kul do od, wah aan bok lingrah vod
+Ahrk fin tey, boziik fun, do fin gein!
+Wo lost fron wah ney dov
+ahrk fin reyliik do jul
+voth aan suleyk wah ronit faal krein
+
+Ahrk fin kel lost prodah,
+Do ved viing ko fin krah,
+Tol fod zeymah win kein meyz fundein!
+Alduin, feyn do jun,
+kruziik vokun staadnau,
+voth aan bahlok wah diivon fin lein!
+
+Nuz aan sul, fent alok,
+fod fin vul dovah nok,
+fen kos nahlot mahfaeraak ahrk ruz!
+Paaz Keizaal fen kos stin nol bein Alduin jot!
+
+Dovahkiin, Dovahkiin
+naal ok zin los vahriin
+wah dein vokul mahfaeraak ahst vaal!
+Ahrk fin norok paal graan
+fod nust hon zindro zaan
+Dovahkiin, fah hin kogaan mu draal!
+"""
